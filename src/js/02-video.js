a1b09eb763add1fb73958098077ac12cb66ba358
Golenko Andrii <golenko.andrey@gmail.com>
2022-08-15 18:58:45 +0300

    task2-3
    task2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle'); 
 
+const iframe = document.querySelector('#vimeo-player');
+const VIDEO_STORAGE_KEY = "videoplayer-current-time";
+const throttleOptim = throttle(onPlay, 1000);
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttleOptim); 
+
+function onPlay(data) {
+    localStorage.setItem(VIDEO_STORAGE_KEY, data.seconds);
+}
+
+const currentTime = localStorage.getItem(VIDEO_STORAGE_KEY);
+
+if (currentTime) {
+    player.setCurrentTime(currentTime)
+};
